7646660f179f3343f8f20d9b1d44168fc95ec807
econsignganesh <ganesh@econsignsoft.com>
2025-09-08 10:20:47 +0530

    changes

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(
 const allowedOrigins = [
   "http://localhost:5173",
   "http://127.0.0.1:5173",
-  "https://test-frontend.onrender.com",
+  "https://adminpanalnew.netlify.app"
 ];
 const corsOptions = {
   origin: function (origin, callback) {
